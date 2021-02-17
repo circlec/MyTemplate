7ed565f4e52103e514eb31322f0fdb23d5f9ab1d
GitHub Action <action@github.com>
2021-02-17 14:30:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/circlec/mytemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/circlec/mytemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.circlec.mytemplate.listeners
 
+import com.github.circlec.mytemplate.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
